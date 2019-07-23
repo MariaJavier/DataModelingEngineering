64522e3841ab5d020d3115e3c4742eb1d9b69af8
MariaJavier <49207251+MariaJavier@users.noreply.github.com>
2019-07-22 20:27:33 -0400

    updated ipynb file with the empty user and password

--- schema.sql
+++ schema.sql
@@ -2,8 +2,7 @@
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
 -- Modify this code to update the DB schema diagram.
--- To reset the sample schema, replace everything with
--- two dots ('..' - without quotes).
+-- Run all of the below code in PGAdmin (PostgreSQL) to create the tables.
 
 CREATE TABLE "departments" (
     "dept_no" VARCHAR(10)   NOT NULL,
